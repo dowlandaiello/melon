ae8cc017fdea8a749550719778209a93c00f033c
Dowland Aiello <mitsukomegumii@gmail.com>
2019-09-07 12:40:51 -0400

    host: Implement UpgradeOptioon and TransportOption.
    Signed-off-by: Dowland Aiello <mitsukomegumii@gmail.com>

--- src/main/java/com/dowlandaiello/melon/host/Host.java
+++ src/main/java/com/dowlandaiello/melon/host/Host.java
@@ -17,6 +17,7 @@ import javax.crypto.NoSuchPaddingException;
 
 import com.dowlandaiello.melon.transport.Tcp;
 import com.dowlandaiello.melon.transport.Transport;
+import com.dowlandaiello.melon.transport.Upgrade;
 import com.dowlandaiello.melon.transport.secio.Secio;
 
 /**
@@ -43,19 +44,79 @@ public class Host {
     }
 
     /**
+     * Represents a configuration option used to specify a certain transport for p2p
+     * communications rather than the default tcp transport.
+     */
+    public static class TransportOption {
+        /**
+         * The transport to use.
+         */
+        private final Transport transport;
+
+        /**
+         * Initializes a new TransportOption with the given transport.
+         * 
+         * @param transport the transport to use
+         */
+        public TransportOption(Transport transport) {
+            this.transport = transport; // Set transport
+        }
+
+        /**
+         * Applies the option to the given host.
+         * 
+         * @param host the host to apply the option to
+         */
+        public void apply(Host host) {
+            host.transport = this.transport; // Set transport
+        }
+    }
+
+    /**
+     * Represents a configuration option used to apply an upgrade to a melon host's
+     * transport. In other words, an UpgradeOption is shorthand for a
+     * TransportOption specifying a transport using .withUpgrade() to apply an
+     * upgrade.
+     */
+    public static class UpgradeOption {
+        /**
+         * The upgrade to apply.
+         */
+        private final Upgrade upgrade;
+
+        /**
+         * Initializes a new UpgradeOption with the given upgrade.
+         * 
+         * @param upgrade the upgrade to apply to the host
+         */
+        public UpgradeOption(Upgrade upgrade) {
+            this.upgrade = upgrade; // Set upgrade
+        }
+
+        /**
+         * Applies the option to the given host.
+         * 
+         * @param host the host to apply the option to
+         */
+        public void apply(Host host) {
+            host.transport = host.transport.withUpgrade(this.upgrade); // Upgrade host's transport
+        }
+    }
+
+    /**
      * The unique identifier of the host.
      */
-    byte[] peerId;
+    public byte[] peerId;
 
     /**
      * The keypair used to derive the peerId of the host.
      */
-    KeyPair keypair;
+    public KeyPair keypair;
 
     /**
      * The transport used to make connections with other peers.
      */
-    Transport transport;
+    public Transport transport;
 
     /**
      * Initializes a new host, and applies all of the given options.
@@ -74,9 +135,9 @@ public class Host {
 
         this.keypair = generator.generateKeyPair(); // Create keypair
         this.peerId = this.keypair.getPublic().getEncoded(); // Hash public key
-        this.transport = new Tcp().withUpgrade(new Secio(this.keypair)); // Initialize a tcp
-                                                                         // transport to make
-                                                                         // connections from
+
+        // Initialize a tcp transport to make connections from
+        this.transport = new Tcp().withUpgrade(new Secio(this.keypair));
 
         // Iterate through provided options
         for (int i = 0; i < opts.length; i++) {
